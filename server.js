5d8b83bcf46c285d5991c45164ef00746f96abbd
Marat Zainullin <maratreason@mail.ru>
2019-05-16 14:18:45 +0400

    envoiroment plugins first start

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express'); 
+require('dotenv').config();
+
+// console.log(process.env);
+
+const app = express();
+
+const mustache = mustacheExpress();
+mustache.cache = null;
+
+app.engine('mustache', mustache);
+app.set('view engine', 'mustache');
+
+app.use(express.static('public'));
+
+app.get('/list', (req, res) => {
+	res.render('list');
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`listening on port ${process.env.PORT}`);
+});
