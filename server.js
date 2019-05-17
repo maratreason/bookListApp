47929de4a03599a3592cb489d9c21ff1146d40d2
Marat Zainullin <maratreason@mail.ru>
2019-05-17 15:05:18 +0400

    add and delete query and change scss file

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express'); 
+const bodyParser = require('body-parser'); 
+const { Client } = require('pg'); 
+const pgCamelCase = require('pg-camelcase'); 
+
+pgCamelCase.inject(require('pg'));
+
 require('dotenv').config();
 
 // console.log(process.env);
@@ -14,12 +20,76 @@ app.set('view engine', 'mustache');
 
 app.use(express.static('public'));
 
-app.get('/list', (req, res) => {
-	res.render('list-page');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/books', (req, res) => {
+
+	const client = new Client();
+	client.connect()
+		.then(() => {
+			return client.query('SELECT * FROM books');
+		})
+		.then((results) => {
+			console.log('results?', results);
+			res.render('book-list', results);
+		})
+		.catch((err) => {
+			console.log('Error: ', err);
+			res.send('Something wrong');
+		})
+
 });
 
-app.get('/book-form', (req, res) => {
+/**
+ * Add Book
+ */
+app.get('/book/add', (req, res) => {
 	res.render('book-form');
+});
+
+app.post('/book/add', (req, res) => {
+	
+	const client = new Client();
+
+	client.connect()
+		.then(() => {
+			console.log('connection complete');
+			
+			const sql = 'insert into books(title, authors) values ($1, $2)';
+			const params = [req.body.title, req.body.authors];
+
+			return client.query(sql, params);
+		})
+		.then((result) => {
+			console.log('result?', result);
+			res.redirect('/books');
+		})
+		.catch((err) => {
+			console.log('err', err);
+			res.redirect('/books');
+		});
+});
+
+app.post('/book/delete/:id', (req, res) => {
+	console.log('delete', req.params.id);
+	const client = new Client();
+
+	client.connect()
+		.then(() => {
+			console.log('connection complete');
+			const sql = 'DELETE FROM books WHERE book_id=$1';
+			const params = [req.params.id];
+			return client.query(sql, params);
+		})
+		.then((results) => {
+			console.log('delete results?', result);
+			res.redirect('/books');
+		})
+		.catch((err) => {
+			console.log('err', err);
+			res.redirect('/books');
+		});
+
 });
 
 app.listen(process.env.PORT, () => {
